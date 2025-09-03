04913a86fcb1acccbc04d9584cec1092244fe2fe
niloyxdbd <niloyxd.bd@gmail.com>
2025-09-03 13:44:22 -0500

    Create products.js

---
+++ pages/api/products.js
@@ -0,0 +1,9 @@
+```js
+import fs from 'fs'
+import path from 'path'
+export default function handler(req,res){
+const p = path.join(process.cwd(),'data/products.json')
+const products = JSON.parse(fs.readFileSync(p,'utf8'))
+res.status(200).json(products)
+}
+```
