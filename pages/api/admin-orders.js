a6566c5ea82109d58160f96766630eef09979cc2
niloyxdbd <niloyxd.bd@gmail.com>
2025-09-04 10:35:21 -0500

    Update admin-orders.js

--- pages/api/admin-orders.js
+++ pages/api/admin-orders.js
@@ -1,5 +1,6 @@
 // pages/api/admin-orders.js
-import { supabase } from '../../../lib/supabase';
+import supabase from '../../lib/supabase';
+
 
 export default async function handler(req, res) {
   const { password } = req.query
